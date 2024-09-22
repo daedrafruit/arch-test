295e65b9d9bee2320c8e69ba67cec6b08a380383
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2024-09-22 16:37:59 -0700

    fixed type

--- arch.bash
+++ arch.bash
@@ -81,13 +81,13 @@ cp vars-config.bash /mtn/root/vars-config.bash
 # Run system configuration script inside chroot
 curl -O https://raw.githubusercontent.com/daedrafruit/arch-test/main/system-config.bash
 chmod +x system-config.bash
-cp system-config.bash /mtn/root/system-config.bash
+cp system-config.bash /mnt/root/system-config.bash
 arch-chroot /mnt /bin/bash -x /root/system-config.bash
 
 # Run user configuration script inside chroot as the specified user
 curl -O https://raw.githubusercontent.com/daedrafruit/arch-test/main/user-config.bash
 chmod +x user-config.bash
-cp user-config.bash /mtn/root/user-config.bash
+cp user-config.bash /mnt/root/user-config.bash
 arch-chroot /mnt /usr/bin/runuser -u $username -c /root/user-config.bash
 
 # Finish up
