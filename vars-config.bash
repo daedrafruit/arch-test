37eb72032f3cd9dd76658d61da8ffd493bd8592d
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2024-09-23 00:01:25 -0700

    added virtual box stuff

--- vars-config.bash
+++ vars-config.bash
@@ -14,10 +14,10 @@ locale="en_US.UTF-8"
 kblayout="us"
 timezone="US/Arizona"
 
-pacstrap_pkgs="base base-devel linux linux-firmware sudo networkmanager grub efibootmgr"
+pacstrap_pkgs="virtualbox-guest-utils base base-devel linux linux-firmware sudo networkmanager grub efibootmgr"
 pacman_pkgs="foot unzip git base-devel stow firefox waybar ranger wofi swaybg kitty flameshot ly ttf-font-awesome"
 aur_pkgs="swayfx"
 
 stow_args="sway waybar wofi kitty flameshot bashrc ranger"
 
-services="NetworkManager systemd-timesyncd ly"
+services="vboxservice NetworkManager systemd-timesyncd ly"
